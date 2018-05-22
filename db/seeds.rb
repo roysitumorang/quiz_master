4ecd8c6b34762891c57167e9af24de8d325f0973
Roy Situmorang <roy.situmorang@gmail.com>
2018-05-22 09:10:34 +0700

    Populate quiz data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Quiz.create([
+  {question: 'How many vowels are there in the English alphabet?', answer: 5},
+  {question: 'How many days in a week?', answer: 7},
+])
